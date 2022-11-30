486074bbeaa590a1549d790071d3cfbd14fa0480
custibor99 <cus.tibor@outlook.com>
2022-11-30 12:05:46 +0100

    Adding healthchecks

--- src/main/java/com/rso/ChargingStation/api/MockApi.java
+++ src/main/java/com/rso/ChargingStation/api/MockApi.java
@@ -1,5 +1,10 @@
 package com.rso.ChargingStation.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
+import org.springframework.boot.availability.LivenessState;
+import org.springframework.boot.availability.ReadinessState;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +17,9 @@ import java.awt.*;
 @RequestMapping("api/v1/mock")
 public class MockApi {
 
+    @Autowired
+    ApplicationContext context;
+
     @GetMapping(value = "/echo", produces = MediaType.APPLICATION_JSON_VALUE)
     public String echoString(@RequestParam final String msg) {
         return msg;
@@ -20,5 +28,29 @@ public class MockApi {
     @GetMapping("/ping")
     public String ping() {
         return "ack";
+    }
+
+    @GetMapping("/health/liveliness/good")
+    public int setLivelinessGood(){
+        AvailabilityChangeEvent.publish(context, LivenessState.CORRECT);
+        return 1;
+    }
+
+    @GetMapping("/health/liveliness/bad")
+    public int setLivelinessBad(){
+        AvailabilityChangeEvent.publish(context, LivenessState.BROKEN);
+        return 1;
+    }
+
+    @GetMapping("/health/rediness/good")
+    public int setReadinessGood(){
+        AvailabilityChangeEvent.publish(context, ReadinessState.ACCEPTING_TRAFFIC);
+        return 1;
+    }
+
+    @GetMapping("/health/rediness/bad")
+    public int setReadinessBad(){
+        AvailabilityChangeEvent.publish(context, ReadinessState.REFUSING_TRAFFIC);
+        return 1;
     }
 }
