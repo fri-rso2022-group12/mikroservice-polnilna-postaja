c887117b105078ac065d4fc118eac9ee3e995c81
custibor99 <cus.tibor@outlook.com>
2022-11-30 21:01:40 +0100

    Adding prometheus metrics

--- src/main/java/com/rso/ChargingStation/api/StationAPI.java
+++ src/main/java/com/rso/ChargingStation/api/StationAPI.java
@@ -2,6 +2,9 @@ package com.rso.ChargingStation.api;
 
 import com.rso.ChargingStation.model.ChargingStation;
 import com.rso.ChargingStation.repository.ChargingStationRepository;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +18,18 @@ public class StationAPI {
 
     @Autowired
     ChargingStationRepository chargingStationRepository;
+    private final Counter apiCallCounter = Metrics.counter("ApiCallCounter");
+
 
     @GetMapping("/")
     public List<ChargingStation> getAllStations() {
+        apiCallCounter.increment();
         return chargingStationRepository.getAllByIdBetween(0l, 100l);
     }
 
     @GetMapping("/{id}")
     public ChargingStation getStationsById(@PathVariable("id") Long id) {
+        apiCallCounter.increment();
         ChargingStation res = chargingStationRepository.getChargingStationById(id);
         if (res == null){
             throw new EntityNotFoundException("No entity with id="+id.toString());
@@ -32,16 +39,19 @@ public class StationAPI {
 
     @GetMapping("/delete/{id}")
     public void deleteStationById(@PathVariable("id") Long id) {
+        apiCallCounter.increment();
         chargingStationRepository.deleteById(id);
     }
 
     @GetMapping("/nearest")
     public ChargingStation getClosestStation(@RequestParam Double x, @RequestParam Double y){
+        apiCallCounter.increment();
         return chargingStationRepository.getNearestStation(x, y);
     }
 
     @PostMapping(path="/add", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
     public void addStation(@RequestBody ChargingStation newChargingStation) {
+        apiCallCounter.increment();
         chargingStationRepository.save(newChargingStation);
     }
 
